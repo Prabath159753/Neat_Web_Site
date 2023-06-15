32c9f3a8ec5a0fbbb0b3f4685d54f16cf736e471
Kavishka <kavishkaprabath21@gmail.com>
2023-06-16 05:20:29 +0530

    updat js file

--- js/main.js
+++ js/main.js
@@ -68,10 +68,10 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 1000, 'easeInOutExpo');
         return false;
     });
-1
+
     
 })(jQuery);
 
